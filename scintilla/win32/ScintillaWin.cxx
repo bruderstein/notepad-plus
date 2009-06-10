5ac8dbd6bab531a523ab2232d561b1f6d1071575
Jocelyn Legault <jocelynlegault@gmail.com>
2009-06-11 01:15:04 +0800

    Inserted the NPPSTART ... NPPEND delimiters to the mouse wheel zoom disable feature in Scintilla.
    Signed-off-by: Jocelyn Legault <jocelynlegault@gmail.com>
    
    Signed-off-by: TBFowler <thell@almostautomated.com>

--- scintilla/win32/ScintillaWin.cxx
+++ scintilla/win32/ScintillaWin.cxx
@@ -658,6 +658,7 @@ sptr_t ScintillaWin::WndProc(unsigned int iMessage, uptr_t wParam, sptr_t lParam
 				wheelDelta = - (-wheelDelta % WHEEL_DELTA);
 
 			if (wParam & MK_CONTROL) {
+				// NPPSTART Joce 06/10/09 DisableMouseWheelZoom
 				if (enableWheelZooming)
 				{
 					// Zoom! We play with the font sizes in the styles.
@@ -668,6 +669,7 @@ sptr_t ScintillaWin::WndProc(unsigned int iMessage, uptr_t wParam, sptr_t lParam
 						KeyCommand(SCI_ZOOMOUT);
 					}
 				}
+				// NPPEND
 			} else {
 				// Scroll
 				ScrollTo(topLine + linesToScroll);
