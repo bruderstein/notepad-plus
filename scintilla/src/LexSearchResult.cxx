fade7536e49e7741fc9f8d913d39dc58b449de8b
Joce <jocelynlegault@gmail.com>
2009-06-25 00:36:33 -0600

    Forgot one warning in release.
    Signed-off-by: Joce <jocelynlegault@gmail.com>

--- scintilla/src/LexSearchResult.cxx
+++ scintilla/src/LexSearchResult.cxx
@@ -69,6 +69,12 @@ static void ColouriseSearchResultLine(SearchResultMarkings* pMarkings, char *lin
 		
 		PLATFORM_ASSERT(lengthLine >= firstTokenLen + 2);
 
+		// NPPSTART Joce 06/25/09 Cleanup
+		#ifdef NDEBUG
+		lengthLine = lengthLine;
+		#endif
+		// NPPEND
+
 		styler.ColourTo(startLine + firstTokenLen, SCE_SEARCHRESULT_DEFAULT);
 
 		for (currentPos = firstTokenLen; lineBuffer[currentPos] != ':' ; currentPos++) PLATFORM_ASSERT(currentPos < lengthLine);
